24a4b2cc363cafa486301869e252222636f861c9
Girish <Girish@DESKTOP-TNNTK1O>
2020-04-07 19:23:29 +0530

    added one more statement in gitTesy2.java

--- GitDemo/src/gitDemoPackage/gitTest2.java
+++ GitDemo/src/gitDemoPackage/gitTest2.java
@@ -5,6 +5,8 @@ public class gitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first test output is here");
+		
+		System.out.println("My second test output is here");
 	}
 
 }
